164494072daf3bdf42e9c410f06aab71b67c3e55
0704282877 <you@example.com>
2019-05-31 10:56:55 +0300

    changes

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+  $("#hide1").click(function(){
+    $("#up").toggle();
+   });
+  });
+
+  $(document).ready(function(){
+    $("#hide2").click(function(){
+      $("#down").toggle();
+    });
+  });
+
+  $("document").ready(function(){
+    $("#hide3").click(function(){
+      $("#left").toggle();
+    });
+  });
+
+  $(document).ready(function(){
+    $(".contactus").click(function(){
+      alert("Hello!!Thank you for submitting your message.");
+    })
+  })
